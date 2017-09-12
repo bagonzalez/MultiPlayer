a6839e3c96c8baae738da02ac4b1e6cd9f7fa108
Bruno Diaz <bagonzalez.sv@gmail.com>
2017-09-11 18:42:09 -0600

    primer commit

---
+++ ServiceMultiGame/ServiceMultiplayer.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ServiceMultiGame
+{
+
+    [ServiceContract(Namespace = "http://ServiceMultiGame", SessionMode = SessionMode.Required,
+                    CallbackContract = typeof(IMultiplayerCallback))]
+    public interface IMultiplayer
+    {
+        [OperationContract(IsOneWay = false)]
+        string RegisterUser(string user);
+        [OperationContract(IsOneWay = false)]
+        string CreateRoom(string room);
+        [OperationContract(IsOneWay = true)]
+        void AddScore(double n, string token);
+        [OperationContract(IsOneWay = true)]
+        void RemoveScore(double n, string token);
+        [OperationContract(IsOneWay = true)]
+        void GetScore(string token);
+        [OperationContract(IsOneWay = true)]
+        void PlayGame(string token, string room);
+        [OperationContract(IsOneWay = false)]
+        string GetRoom();
+        [OperationContract(IsOneWay = true)]
+        void KeyUp(string token);
+        [OperationContract(IsOneWay = true)]
+        void KeyDown(string token);
+        [OperationContract(IsOneWay = true)]
+        void Register();
+
+    }
+
+    public interface IMultiplayerCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void ResultScore(double result, string token);
+        [OperationContract(IsOneWay = true)]
+        void ResultMsg(string eqn);
+        [OperationContract(IsOneWay = true)]
+        void GetPosition(double result);
+        [OperationContract(IsOneWay = true)]
+        void KeyUp(string token);
+        [OperationContract(IsOneWay = true)]
+        void KeyDown(string token);
+    }
+
+
+    public class ServiceMultiplayer : IMultiplayer
+    {
+        private static List<Jugador> listJugadores = new List<Jugador>();
+        private static List<Room> listRoom = new List<Room>();
+        private static List<IMultiplayerCallback> clientList = new List<IMultiplayerCallback>();
+
+        public void AddScore(double n, string token)
+        {
+            foreach (Jugador jugador in ServiceMultiplayer.listJugadores)
+            {
+                if (jugador.id == token)
+                {
+                    jugador.score += n;
+                    Callback.ResultScore(jugador.score, token);
+                }
+            }
+        }
+
+        public string CreateRoom(string room)
+        {
+            Room newRoom = new Room();
+
+            Random rnd = new Random();
+            string posibles = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            int longitud = posibles.Length;
+            char letra;
+            int longitudnuevacadena = 5;
+            string nuevacadena = "";
+            for (int i = 0; i < longitudnuevacadena; i++)
+            {
+                letra = posibles[rnd.Next(longitud)];
+                nuevacadena += letra.ToString();
+            }
+
+            newRoom.id = nuevacadena;
+            newRoom.name = room;
+            newRoom.player1 = "";
+            newRoom.player2 = "";
+            newRoom.gameStart = false;
+
+            ServiceMultiplayer.listRoom.Add(newRoom);
+            Callback.ResultMsg("Room creado: " + newRoom.name);
+
+            return newRoom.id;
+        }
+
+        public void GetScore(string token)
+        {
+            foreach (Jugador jugador in ServiceMultiplayer.listJugadores)
+            {
+                if (jugador.id == token)
+                {                    
+                    Callback.ResultScore(jugador.score, token);
+                }
+            }
+        }
+
+        public void PlayGame(string token, string room)
+        {
+            foreach (Room roomPlay in ServiceMultiplayer.listRoom)
+            {
+                if (!roomPlay.gameStart)
+                {
+                    if (roomPlay.id == room)
+                    {
+                        if (roomPlay.player1 == "")
+                        {
+                            roomPlay.player1 = token;
+                            Callback.ResultMsg("Player 1 Jugador " + token + " añadido al room " + roomPlay.id);
+                        }
+                        else if (roomPlay.player2 == "")
+                        {
+                            roomPlay.player2 = token;
+                            Callback.ResultMsg("Player 2 Jugador " + token + " añadido al room " + roomPlay.id);
+                        }
+
+                        if (roomPlay.player1 != "" && roomPlay.player2 != "")
+                        {
+                            roomPlay.gameStart = true;
+                            Callback.ResultMsg(" Game start ");
+                        }
+                            
+                    }
+
+                }                
+            }
+        }
+
+        public string RegisterUser(string user)
+        {
+            Jugador jugador = new Jugador();
+
+            Random rnd = new Random();
+            string posibles = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            int longitud = posibles.Length;
+            char letra;
+            int longitudnuevacadena = 5;
+            string nuevacadena = "";
+            for (int i = 0; i < longitudnuevacadena; i++)                
+            {                
+                letra = posibles[rnd.Next(longitud)];                
+                nuevacadena += letra.ToString();                
+            }
+
+            jugador.id = nuevacadena; 
+            jugador.name = user;
+            jugador.score = 0;
+
+            ServiceMultiplayer.listJugadores.Add(jugador);
+            Callback.ResultMsg("Jugador creado: "+ jugador.name);
+
+            return jugador.id;
+        }
+
+        public void RemoveScore(double n, string token)
+        {
+            foreach (Jugador jugador in ServiceMultiplayer.listJugadores)
+            {
+                if (jugador.id == token)
+                {
+                    jugador.score -= n;
+                    Callback.ResultScore(jugador.score, token);
+                }
+            }
+        }
+
+        public string GetRoom()
+        {
+            foreach (Room room in ServiceMultiplayer.listRoom)
+            {
+                if (room.player1 == "")
+                {
+                    Callback.ResultMsg("Obteniendo room "+ room.name);
+                    return room.id;
+                }
+
+                if (room.player2 == "")
+                {
+                    Callback.ResultMsg("Obteniendo room " + room.name);
+                    return room.id;
+                }
+            }
+
+            return "";
+        }
+
+        public void KeyUp(string token)
+        {
+            foreach (IMultiplayerCallback callback in ServiceMultiplayer.clientList)
+            {               
+                callback.KeyUp(token);
+            }
+        }
+
+        public void KeyDown(string token)
+        {
+            foreach (IMultiplayerCallback callback in ServiceMultiplayer.clientList)
+            {
+                
+                callback.KeyDown(token);
+            }            
+        }
+
+        public void Register()
+        {
+            IMultiplayerCallback callback=OperationContext.Current.GetCallbackChannel<IMultiplayerCallback>();
+            ServiceMultiplayer.clientList.Add(callback);
+        }
+
+        IMultiplayerCallback Callback
+        {
+            get
+            {
+                return OperationContext.Current.GetCallbackChannel<IMultiplayerCallback>();
+            }
+        }
+    }
+
+    public class Jugador
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double score { get; set; }
+
+        public Jugador() { }
+
+    }
+
+    public class Room
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string player1 { get; set; }
+        public string player2 { get; set; }
+        public bool gameStart { get; set; }
+        public Room() { }
+
+    }
+}
---
+++ client/client.cs
@@ -0,0 +1,109 @@
+﻿
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.
+
+using System;
+using System.ServiceModel;
+
+namespace ServiceMultiGame
+{
+
+    public class CallbackHandler : IMultiplayerCallback
+    {
+     
+        public void ResultScore(double result, string token)
+        {
+            Console.WriteLine("Result({0} toke {1})", result, token);
+        }
+
+        public void ResultMsg(string eqn)
+        {
+            Console.WriteLine("MSJ({0})", eqn);
+        }
+
+        public void GetPosition(double result)
+        {
+            Console.WriteLine("Result({0})", result);
+        }
+
+        public void KeyUp(string token)
+        {
+            Console.WriteLine("Tecla arriba " +"usuario "+ token);
+        }
+
+        public void KeyDown(string token)
+        {
+            Console.WriteLine("Tecla Abajo " + "usuario " + token);
+        }
+    }
+
+
+    class Client
+    {
+        static void Main()
+        {
+            // Construct InstanceContext to handle messages on callback interface
+            InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+
+            // Create a client
+            ServiceMultiplayerClient client = new ServiceMultiplayerClient(instanceContext);
+
+            Console.WriteLine("Press <ENTER> to terminate client once the output is displayed.");
+            Console.WriteLine();
+
+            // Call the AddTo service operation.
+
+            client.Register();
+
+
+            string  player1=client.RegisterUser("Bruno");
+            
+            
+            string room=client.GetRoom();
+            if (room == "")
+            {
+                client.CreateRoom("banana");
+            }
+
+            client.PlayGame(player1, room);
+
+            //string player2Token= client.RegisterUser("Eduardo");
+
+            //client.PlayGame(player2Token, room);
+
+
+            //client.KeyUp(player2Token);
+
+            // string room = client.GetRoom();
+
+            Console.WriteLine("room" +room);
+
+
+            client.AddScore(200, player1);
+            client.RemoveScore(500, player1);
+
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    if (Console.ReadKey(true).Key == ConsoleKey.UpArrow)
+                    {
+                        client.KeyUp(player1);
+                    }
+
+                    if (Console.ReadKey(true).Key == ConsoleKey.DownArrow)
+                    {
+                        client.KeyDown(player1);
+                    }
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            Console.ReadLine();
+
+            //Closing the client gracefully closes the connection and cleans up resources
+            client.Close();
+        }
+    }
+
+
+    }
+
